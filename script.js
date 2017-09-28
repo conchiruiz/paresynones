46ae2166b690c7bf1424f46290d5528b0ac5da0e
conchiruiz <conchirlopez6918@gmail.com>
2017-09-28 14:57:26 +0200

    nuevo cambio

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('no').onclick=function(){
+    document.getElementById('bievenida)').style.display="none";
+    document.getElementById('nojuega').style.display="block";
+}
+    document.getElementById('si').onclick=function(){
+    document.getElementById('bienvenida').style.display="none";
+    document.getElementById('paroimpar)').style.display="block";
+    }
